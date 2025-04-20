b0e8737c2794836b785957bbd86a70013a424f24
Simon PABOT <simonpabot@outlook.com>
2025-04-20 18:31:08 +0200

    v0.0.4

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,1 +1,1 @@
-export { is } from "./typia";
+export { is } from "./is";
--- src/handlers/is.ts
+++ src/handlers/is.ts
@@ -1,11 +1,12 @@
 import type { IResponse, IResponseHandler } from "..";
+import type { IValidation } from "typia";
 
 export interface IResponseHandlerIs<
   T,
   Headers extends Partial<IResponse["headers"]>,
   Body extends unknown,
 > {
-  headers: (input: IResponse["headers"]) => Headers | null;
+  headers: (input: IResponse["headers"]) => IValidation<Headers>;
   body: (input: string) => Body | null;
   handle(resp: { headers: Headers; body: Body }): T | null;
 }
@@ -15,11 +16,11 @@ export function is<T, Headers extends Partial<IResponse["headers"]>, Body>(
 ): IResponseHandler<T> {
   return async function (resp: IResponse) {
     const headers = opts.headers(resp.headers);
-    if (!headers) return null;
+    if (!headers.success) return null;
 
     const body = opts.body(await resp.downloadBody());
     if (body === null) return null;
 
-    return opts.handle({ headers, body });
+    return opts.handle({ headers: headers.data, body });
   };
 }
